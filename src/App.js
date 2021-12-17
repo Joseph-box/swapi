3545bee38600f5b5d9f80281aabcd2770a3ef888
Joseph Azagoury <64476475+Joseph-box@users.noreply.github.com>
2021-12-17 12:25:04 +0200

    progress counter and catch

--- src/App.js
+++ src/App.js
@@ -4,8 +4,15 @@ import "./App.css";
 
 function App() {
 	const [part1Data, setPart1Data] = useState(null);
+	const [part1Step, setPart1Step] = useState(0);
+
 	const [part2Data, setPart2Data] = useState(null);
+	const [part2Step, setPart2Step] = useState(0);
+
 	const apiURL = "https://www.swapi.tech/api/";
+
+	let errorCounter = 0;
+	const [errorText, setErrorText] = useState(null);
 
 	let allShips = [];
 	let allShipsDetails = [];
@@ -42,25 +49,55 @@ function App() {
 		return await axios.all(getList);
 	};
 
-	const getShipList = async () => {
-		await getAll("vehicles").then((response) => {
-			allShips = response.data.results;
+	const errorFound = async (err) => {
+		errorCounter++;
+		setErrorText("Error: Retry again later");
+		console.log(err);
+		console.log("Retry again later.");
+	};
+
+	const part1StepCounter = () => {
+		setPart1Step((part1Step) => {
+			return part1Step + 1;
 		});
 	};
 
+	// Part1 Step1
+	const getShipList = async () => {
+		await getAll("vehicles")
+			.then((response) => {
+				allShips = response.data.results;
+				part1StepCounter();
+			})
+			.catch(async (err) => {
+				errorFound(err);
+			});
+	};
+
+	// Part1 Step2
 	const getShipDetails = async () => {
+		if (errorCounter > 0) return;
+
 		let idList = [];
 
 		allShips.forEach((ship) => {
 			idList.push(ship.uid);
 		});
 
-		await getMultipleDetailsId("vehicles", idList).then((response) => {
-			allShipsDetails = response;
-		});
+		await getMultipleDetailsId("vehicles", idList)
+			.then((response) => {
+				allShipsDetails = response;
+				part1StepCounter();
+			})
+			.catch(async (err) => {
+				errorFound(err);
+			});
 	};
 
+	// Part1 Step3
 	const getPilotsDetails = async () => {
+		if (errorCounter > 0) return;
+
 		let pilotList = [];
 
 		allShipsDetails.forEach((ship) => {
@@ -74,33 +111,49 @@ function App() {
 			}
 		});
 
-		await getMultipleDetailsURLs(pilotList).then(
-			axios.spread((...responses) => {
-				responses.forEach((pilotDetail, index) => {
-					shipsAndPilots[index].pilot = pilotDetail.data.result.properties;
-				});
-			})
-		);
+		await getMultipleDetailsURLs(pilotList)
+			.then(
+				axios.spread((...responses) => {
+					responses.forEach((pilotDetail, index) => {
+						shipsAndPilots[index].pilot = pilotDetail.data.result.properties;
+					});
+					part1StepCounter();
+				})
+			)
+			.catch(async (err) => {
+				errorFound(err);
+			});
 	};
 
+	// Part1 Step4
 	const getHomeworldDetails = async () => {
+		if (errorCounter > 0) return;
+
 		let homeworldList = [];
 
 		shipsAndPilots.forEach((ship) => {
 			homeworldList.push(ship.pilot.homeworld);
 		});
 
-		await getMultipleDetailsURLs(homeworldList).then(
-			axios.spread((...responses) => {
-				responses.forEach((homeworldDetail, index) => {
-					shipsAndPilots[index].homeworld =
-						homeworldDetail.data.result.properties;
-				});
-			})
-		);
+		await getMultipleDetailsURLs(homeworldList)
+			.then(
+				axios.spread((...responses) => {
+					responses.forEach((homeworldDetail, index) => {
+						shipsAndPilots[index].homeworld =
+							homeworldDetail.data.result.properties;
+					});
+					part1StepCounter();
+				})
+			)
+			.catch(async (err) => {
+				errorFound(err);
+			});
 	};
 
+	// Part1 Step5.1
 	const addNewShip = (newShip) => {
+		if (errorCounter > 0) return;
+
 		shipsAndPopulation.push({
 			ship: newShip.ship,
 			pilots: [newShip.pilot.name],
@@ -109,7 +162,10 @@ function App() {
 		});
 	};
 
+	// Part1 Step5.2
 	const addPilotToShip = (ship, newData) => {
+		if (errorCounter > 0) return;
+
 		let newPlanet = true;
 
 		ship.pilots.push(newData.pilot.name);
@@ -126,7 +182,10 @@ function App() {
 		}
 	};
 
+	// Part1 Step5
 	const populationPerShip = () => {
+		if (errorCounter > 0) return;
+
 		if (shipsAndPilots.length > 0) {
 			let newPilot = false;
 
@@ -145,9 +204,13 @@ function App() {
 				}
 			}
 		}
+		part1StepCounter();
 	};
 
+	// Part1 Step6
 	const seletShipWithMostPopulation = () => {
+		if (errorCounter > 0) return;
+
 		let selectedShip = shipsAndPopulation[0];
 
 		shipsAndPopulation.forEach((ship) => {
@@ -155,10 +218,11 @@ function App() {
 				selectedShip = ship;
 			}
 		});
-
+		part1StepCounter();
 		setPart1Data(selectedShip);
 	};
 
+	// Part1
 	const part1 = async () => {
 		await getShipList(); // includes ships IDs
 		await getShipDetails(); // includes pilots URLs
@@ -169,30 +233,55 @@ function App() {
 		seletShipWithMostPopulation();
 	};
 
-	const getPlanetsList = async () => {
-		await getAll("planets").then((response) => {
-			planetsList = response.data.results;
+	const part2StepCounter = () => {
+		setPart2Step((part2Step) => {
+			return part2Step + 1;
 		});
 	};
 
+	// Part2 Step1
+	const getPlanetsList = async () => {
+		if (errorCounter > 0) return;
+
+		await getAll("planets")
+			.then((response) => {
+				planetsList = response.data.results;
+				part2StepCounter();
+			})
+			.catch(async (err) => {
+				errorFound(err);
+			});
+	};
+
+	// Part2 Step2
 	const getPlanetsDetails = async () => {
+		if (errorCounter > 0) return;
+
 		let urlList = [];
 
 		planetsList.forEach((planet) => {
 			urlList.push(planet.url);
 		});
 
-		await getMultipleDetailsURLs(urlList).then(
-			axios.spread((...responses) => {
-				responses.forEach((planetDetail, index) => {
-					planetsList[index].population =
-						planetDetail.data.result.properties.population;
-				});
-			})
-		);
+		await getMultipleDetailsURLs(urlList)
+			.then(
+				axios.spread((...responses) => {
+					responses.forEach((planetDetail, index) => {
+						planetsList[index].population =
+							planetDetail.data.result.properties.population;
+					});
+					part2StepCounter();
+				})
+			)
+			.catch(async (err) => {
+				errorFound(err);
+			});
 	};
 
+	// Part2 Step3
 	const selectPlanetsToDisplay = () => {
+		if (errorCounter > 0) return;
+
 		let planets = ["Tatooine", "Alderaan", "Naboo", "Bespin", "Endor"];
 		let maxPopulation = 0;
 		let graphData = [];
@@ -214,7 +303,7 @@ function App() {
 			planet.percentage = (planet.population * 100) / maxPopulation;
 		});
 
-		// console.log(graphData);
+		part2StepCounter();
 		setPart2Data(graphData);
 	};
 
@@ -224,55 +313,70 @@ function App() {
 		selectPlanetsToDisplay();
 	};
 
-	// run
+	// Run once
 	useEffect(() => {
+		errorCounter = 0;
 		part1();
 		part2();
 	}, []);
 
 	return (
 		<div className="App">
-			{part1Data !== null ? (
-				<div className="Wrapper">
-					<div className="Row">
-						<span className="Square">Vehicle name with the largest sum</span>
-						<span className="Square">{part1Data.ship}</span>
-					</div>
-					<div className="Row">
-						<span className="Square">
-							Related home planets and their respective population
-						</span>
-						<span className="Square">
-							{part1Data.planets.map((planet) => (
-								<div>
-									{planet.name} ({planet.population})
+			{!errorText ? (
+				<>
+					<div className="part-1">Part 1</div>
+					{part1Data !== null ? (
+						<div className="Wrapper">
+							<div className="Row">
+								<span className="Square">
+									Vehicle name with the largest sum
+								</span>
+								<span className="Square">{part1Data.ship}</span>
+							</div>
+							<div className="Row">
+								<span className="Square">
+									Related home planets and their respective population
+								</span>
+								<span className="Square">
+									{part1Data.planets.map((planet) => (
+										<div key={planet.name}>
+											{planet.name} ({planet.population})
+										</div>
+									))}
+								</span>
+							</div>
+							<div className="Row">
+								<span className="Square">Related pilot names</span>
+								<span className="Square">{part1Data.pilots.join(", ")}</span>
+							</div>
+						</div>
+					) : (
+						<div>{((part1Step * 100) / 6).toFixed(2)}% Completed</div>
+					)}
+					<div className="part-2">Part 2</div>
+					{part2Data !== null ? (
+						<div className="graph-wrapper">
+							{part2Data.map((planet) => (
+								<div key={planet.name} className="graph-planet">
+									<span>{planet.population}</span>
+									<span
+										style={{
+											backgroundColor: "yellow",
+											height: `${planet.percentage}%`,
+											border: "1px solid black",
+										}}
+									></span>
+									<span>{planet.name}</span>
 								</div>
 							))}
-						</span>
-					</div>
-					<div className="Row">
-						<span className="Square">Related pilot names</span>
-						<span className="Square">{part1Data.pilots}</span>
-					</div>
-				</div>
-			) : null}
-			{part2Data !== null ? (
-				<div className="graph-wrapper">
-					{part2Data.map((planet) => (
-						<div className="graph-planet">
-							<span>{planet.population}</span>
-							<span
-								style={{
-									backgroundColor: "yellow",
-									height: `${planet.percentage}%`,
-									border: "1px solid black",
-								}}
-							></span>
-							<span>{planet.name}</span>
 						</div>
-					))}
-				</div>
-			) : null}
+					) : (
+						<div>{((part2Step * 100) / 3).toFixed(2)}% Completed</div>
+					)}
+				</>
+			) : (
+				<div>{errorText}</div>
+			)}
 		</div>
 	);
 }
